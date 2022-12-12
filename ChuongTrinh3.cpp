2bd826af5d8dd4441136f283004cc5ef35e78a42
TranThiDieuAis <102138007+TranThiDieuAis@users.noreply.github.com>
2022-12-12 12:54:35 +0700

    Chương trình 3

---
+++ ChuongTrinh3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+	printf(" 		(a) Press 0 to Exit.\n");
+	printf("		(b) Press 1 to Login as admin.\n");
+	printf("		(c) Press 2 to Register as admin.\n");
+	printf("		(d) Press 3 to Login as Passenger.\n");
+	printf("		(e) Press 4 to Register as Passenger.\n");
+	printf("		(f) Press 5 to Display the User Manual.\n");
+	printf("		Enter the desired option:     3\n");
+	
+	printf(" .o88b. db    db .d8888. d888888b  .d88b.  .88b  d88. d88888b d8888b.      db       .d88b.   d888b  d888888b d8b   db\n");
+	printf("d8P  Y8 88    88 88'  YP `~~88~~' .8P  Y8. 88'YbdP`88 88'     88  `8D      88      .8P  Y8. 88' Y8b   `88'   888o  88\n");
+	printf("8P      88    88 `8bo.      88    88    88 88  88  88 88ooooo 88oobY'      88      88    88 88         88    88V8o 88\n");
+	printf("8b      88    88   `Y8b.    88    88    88 88  88  88 88~~~~~ 88`8b        88      88    88 88  ooo    88    88 V8o88\n");
+	printf("Y8b  d8 88b  d88 db   8D    88    `8b  d8' 88  88  88 88.     88 `88.      88booo. `8b  d8' 88. ~8~   .88.   88  V888\n");
+	printf(" `Y88P' ~Y8888P' `8888Y'    YP     `Y88P'  YP  YP  YP Y88888P 88   YD      Y88888P  `Y88P'   Y888P  Y888888P VP   V8P\n");
+	
+	
+	
+	
+	
+	printf("\nEnter the Email to Login :    someone@gmail.com\t\n");
+	printf("Enter the Password :   hithere123\n");
+	 
+	printf("\n			Logged in Successfully as ..... For further Proceedings, enter a value from below....");
+	
+	printf("\n				+++++++++ 3rd Layer Menu +++++++++						Logged in as \n");
+	
+	printf("		(a) Enter 1 to Book a flight....\n");
+	printf("		(b) Enter 2 to update your Data....\n");
+	printf("		(c) Enter 3 to delete your account....\n");
+	printf("		(d) Enter 4 to Display Flight Schedule....\n");
+	printf("		(e) Enter 5 to Cancel a Flight....\n");
+	printf("		(f) Enter 6 to Display all flights registered by....\n");
+	printf("		(g) Enter 0 to Go back to the Main Menu/Logout....\n");
+	printf("		Enter the desired Choice :   ");
+	
+	
+}
