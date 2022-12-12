5f7e5056bc0ce571244885fcb307c94afb38ed04
TranThiDieuAis <102138007+TranThiDieuAis@users.noreply.github.com>
2022-12-12 12:48:49 +0700

    Add files via upload

---
+++ ChuongTrinh2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	printf(" 		(a) Press 0 to Exit.\n");
+	printf("		(b) Press 1 to Login as admin.\n");
+	printf("		(c) Press 2 to Register as admin.\n");
+	printf("		(d)Press 3 to Login as Passenger.\n");
+	printf("		(e) Press 4 to Register as Passenger.\n");
+	printf("		(f) Press 5 to Display the User Manual.\n");
+	printf("		Enter the desired option:     4\n");
+	
+	printf("\n .o88b. db    db .d8888. d888888b  .d88b.  .88b  d88. d88888b d8888b.      .d8888. d888888b  d888b  d8b   db db    db d8888b.\n");
+    printf("d8P  Y8 88    88 88'  YP `~~88~~' .8P  Y8. 88'YbdP`88 88'     88  `8D      88'  YP   `88'   88' Y8b 888o  88 88    88 88  `8D\n");     
+    printf("8P      88    88 `8bo.      88    88    88 88  88  88 88ooooo 88oobY'      `8bo.      88    88      88V8o 88 88    88 88oodD'\n");      
+    printf("8b      88    88   `Y8b.    88    88    88 88  88  88 88~~~~~ 88`8b          `Y8b.    88    88  ooo 88 V8o88 88    88 88~~~  \n");        
+    printf("Y8b  d8 88b  d88 db   8D    88    `8b  d8' 88  88  88 88.     88 `88.      db   8D   .88.   88. ~8~ 88  V888 88b  d88 88     \n");        
+    printf(" `Y88P' ~Y8888P' `8888Y'    YP     `Y88P'  YP  YP  YP Y88888P 88   YD      `8888Y' Y888888P  Y888P  VP   V8P ~Y8888P' 88     \n");    	 
+
+	
+	
+	printf("\n\n\n%30s ++++++++++++++ Welcome to the Customer Registration Portal ++++++++++++++", "");
+	printf("\nEnter your name :     Salman");
+	printf("\nEnter your email address :     someone@gmail.com\t");
+	printf("\nEnter your Password :    hithere123\t");
+	printf("\nEnter your Phone number :     032890383\t");
+	printf("\nEnter your address :   Pakistan\t");
+	printf("\nEnter your age :    19\t");
+	
+	
+
+}
